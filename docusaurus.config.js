326cde87787fd9b878e8e0b00a8bc3fb66e318d8
SanniM3 <sannimardhiyah@gmail.com>
2021-02-22 09:07:19 +0100

    removed lunr search plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,9 +8,7 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
-  plugins: [  
-    '@aldridged/docusaurus-plugin-lunr'
-  ],
+  
   themeConfig: {
     navbar: {
       title: 'My Site',
