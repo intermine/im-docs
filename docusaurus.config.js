cc73f4ede3d6aa4b357d882e2c34ddec85e9bb7d
SanniM3 <sannimardhiyah@gmail.com>
2021-02-24 14:21:34 +0100

    changes logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ module.exports = {
       title: 'InterMine Documentation',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
