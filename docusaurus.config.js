67601f8a2018c755ca1027cccf729cba44a3e421
SanniM3 <sannimardhiyah@gmail.com>
2021-02-24 15:09:17 +0100

    added blog and docs repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,13 +108,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/intermine/im-docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/intermine/blog-container',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
